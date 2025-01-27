1c624e180f505569c996e63e63a38a9f50744826
ChrisJoshRoberts <chrisrobertscollective@gmail.com>
2025-01-27 18:32:44 +0200

    updating logo on auth screens

--- screens/Auth/LoginScreen.js
+++ screens/Auth/LoginScreen.js
@@ -15,7 +15,7 @@ const LoginScreen = ({navigation}) => {
       <SafeAreaView style={styles.container}>
         <View style={styles.authCard}>
           <View style={{width: '100%', alignItems: 'center'}}>
-            <Image source={logo} resizeMode='contain' style={{ width: 80,height: 50}} />
+            <Image source={logo} resizeMode='contain' style={{ width: 100,height: 50}} />
           </View>
           <AuthContent isLogin />
         </View>
--- screens/Auth/SignUpScreen.js
+++ screens/Auth/SignUpScreen.js
@@ -16,7 +16,7 @@ const SignUpScreen = ({navigation}) => {
       <SafeAreaView style={styles.container}>
         <View style={styles.authCard}>
           <View style={{width: '100%', alignItems: 'center'}}>
-            <Image source={logo} resizeMode='contain' style={{ width: 80,height: 50}} />
+            <Image source={logo} resizeMode='contain' style={{ width: 100,height: 50}} />
           </View>
           <AuthContent />
         </View>
